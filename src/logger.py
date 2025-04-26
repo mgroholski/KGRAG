70219cf13ab38a9a1ca07c06d567f8818ee1738a
Matthew Groholski <mgrohols@asu.edu>
2025-04-26 08:47:07 -0700

    Changed logger to print

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,18 @@ class Logger:
     def __init__(self, writepath):
         try:
             print(f"Opening output file at {writepath}.")
-            self.write_file = open(writepath, "w")
+            # self.write_file = open(writepath, "w")
         except Exception as e:
             print("Could not open output file.", e)
             exit()
 
     def log(self, msg):
-        self.write_file.write(msg + "\n")
+        print(msg)
+        # self.write_file.write(msg + "\n")
 
     def close(self):
         print("Closing output file...")
-        self.write_file.close()
+        # self.write_file.close()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
